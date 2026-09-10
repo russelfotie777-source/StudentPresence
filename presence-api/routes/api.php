f09fb39e1771083176781bb2795c43ce69729ce7
CS7-Sorelatangana <sorelatangana04@gmail.com>
2026-09-10 05:01:30 +0100

    Ajoute verification temporelle et rate limiting sur le check-in

--- presence-api/routes/api.php
+++ presence-api/routes/api.php
@@ -105,9 +105,9 @@ Route::middleware(['auth:sanctum', 'validated', 'face-verified'])->group(functio
     Route::post('/seances/{seance}/mark-prof', [SeanceController::class, 'markProf']);
     Route::post('/seances/{seance}/push', [SeanceController::class, 'push']);
 
-    Route::post('/seances/{seance}/position', [PositionController::class, 'store']);
+    Route::post('/seances/{seance}/position', [PositionController::class, 'store'])->middleware('throttle:20,1');
 
-    Route::post('/seances/{seance}/check-in', [PresenceController::class, 'checkIn']);
+    Route::post('/seances/{seance}/check-in', [PresenceController::class, 'checkIn'])->middleware('throttle:20,1');
     Route::get('/seances/{seance}/roster', [PresenceController::class, 'roster']);
     Route::post('/seances/{seance}/confirm-roster', [PresenceController::class, 'confirmRoster']);
     Route::get('/seances/{seance}/presence-list.pdf', [PdfController::class, 'presenceList']);
